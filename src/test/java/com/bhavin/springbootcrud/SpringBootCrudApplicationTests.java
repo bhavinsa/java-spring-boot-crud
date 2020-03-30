d2a7faa6a9629e45b0ac49983f63b0feae4b6d7b
bhavinsa <bhavinpatelsa@gmail.com>
2020-03-30 09:18:55 +0530

    first commit

---
+++ src/test/java/com/bhavin/springbootcrud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavin.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
